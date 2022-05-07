571ade6f355d9429f6c1968bf1ad539a9d901351
AlisArch <alfrednguyen02@gmail.com>
2022-05-07 19:46:58 +0200

    init board with numbers

--- board/board.py
+++ board/board.py
@@ -1,2 +1,18 @@
 
-board 
+board = []
+
+counter = 0
+
+for r in range(8):
+    row = []
+    for c in range(8):
+        row.append(counter)
+        counter += 1
+
+    board.append(row)
+
+
+
+# board[0][0] - unten links
+# board[7][7] - oben rechts 
+# board y  x 
