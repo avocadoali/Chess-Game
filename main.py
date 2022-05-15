e3b6b646af173710dc42d1936944e97281a2f449
AlisArch <alfrednguyen02@gmail.com>
2022-05-15 20:30:25 +0200

    bishop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from board import Board
+from game_logic.bishop import Bishop
 from game_logic.pawn import Pawn
 from game_logic.piece import Piece
 from game_logic.rook import Rook
@@ -9,22 +10,31 @@ def main():
     board = Board()
     e_board = Board()
 
-    newrook2 = Rook( "AA", "W", 3, 5)
-
-    newrook = Rook( "RB", "W", 5, 5 )
-    newrook3 = Rook( "RB", "", 3, 6 )
-    newrook4 = Rook( "RB", "B", 6, 5 )
-    newrook5 = Rook( "RB", "W", 0, 5 )
-
-
+    bishop1 = Bishop( "BW", "W", 3, 5)
+    newrook2 = Rook( "AA", "B", 5, 7)
+    newrook = Rook( "RB", "B", 7, 1 )
     board.insert(newrook)
     board.insert(newrook2)
-    board.insert(newrook4)
-    board.insert(newrook3)
-    print("")
+    board.insert(bishop1)
     board.print()
-
     board.check_for_field(3,5)
+
+
+    #newrook2 = Rook( "AA", "W", 3, 5)
+    #newrook = Rook( "RB", "W", 5, 5 )
+    #newrook3 = Rook( "RB", "", 3, 6 )
+    #newrook4 = Rook( "RB", "B", 6, 5 )
+    #newrook5 = Rook( "RB", "W", 0, 5 )
+
+
+    #board.insert(newrook)
+    #board.insert(newrook2)
+    #board.insert(newrook4)
+    #board.insert(newrook3)
+    #print("")
+    #board.print()
+
+    #board.check_for_field(3,5)
 
 #    newrook2.put(board.board)
 #    newrook.put(board.board)
