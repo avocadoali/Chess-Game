4ffec4600fc3ebfcbae12752830ff0fcd4661bfd
AlisArch <alfrednguyen02@gmail.com>
2022-05-16 23:51:00 +0200

    knight

--- knight.py
+++ knight.py
@@ -1,40 +1,21 @@
-
+from game_logic.piece import Piece
 from board import Board
 from game_logic.crossed_p import Crossed_P
-from game_logic.piece import Piece
 
-import copy
-
-class Bishop(Piece):
+class Knight(Piece):
     def __init__(self, name, color, pos_x, pos_y):
         super().__init__(name, color, pos_x, pos_y)
 
 
-    def check(self, board):
-        e_board = Board()
-        e_board.board = copy.deepcopy(board)
-       queen self.check_up_right(e_board.board)
-        self.check_up_left(e_board.board)
-        self.check_down_left(e_board.board)
-        self.check_down_right(e_board.board)
-        print("")
-        e_board.print()
-        return e_board
-
     def check_up_right(self, board):
-        pos_y = self.pos_y + 1
+        pos_y = self.pos_y + 2 
         pos_x = self.pos_x + 1
         crossed_piece = Crossed_P()
 
-        while (pos_y <= 7 and pos_x <= 7 and (self.color != board[pos_x][pos_y].color)):
-            if board[pos_x][pos_y].name != "--":
-                board[pos_x][pos_y] = crossed_piece
-                break
-
+        if board[pos_x][pos_y].name != "--":
             board[pos_x][pos_y] = crossed_piece
-            pos_y = pos_y + 1
-            pos_x = pos_x + 1
-        return 0
+
+        board[pos_x][pos_y] = crossed_piece
 
     def check_up_left(self, board):
         pos_y = self.pos_y + 1
