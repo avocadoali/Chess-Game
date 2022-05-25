4cc6fd7f06414ea9cbe6501b3cb9ae940bfd392a
AlisArch <alfrednguyen02@gmail.com>
2022-05-25 02:28:17 +0200

    pawn en passent

--- game_logic/pawn.py
+++ game_logic/pawn.py
@@ -18,12 +18,15 @@ class Pawn(Piece):
         self.check_up(e_board.board)
         self.check_up_right(e_board.board)
         self.check_up_left(e_board.board)
+        self.check_en_passant_up_right(e_board.board)
+        self.check_en_passant_up_left(e_board.board)
+
         print("")
         e_board.print()
         return e_board
 
     def check_up(self, board):
-        double_steps = False
+        steps = False
                 # Check if pawn is in front row
         # for double steps on the first move
         if self.color == "W" and self.pos_y == 1:
@@ -40,7 +43,7 @@ class Pawn(Piece):
             board[pos_x][pos_y+1] = crossed_piece
 
             # Check if second step foward is possible
-            if pos_y+2 <= 7 and board[pos_x][pos_y+2].name == "--":
+            if steps and pos_y+2 <= 7 and board[pos_x][pos_y+2].name == "--":
                 board[pos_x][pos_y+2] = crossed_piece
 
         return 0 
@@ -61,3 +64,26 @@ class Pawn(Piece):
         if (pos_y <= 7 and pos_x >= 0 and board[pos_x][pos_y].name != "--" and self.color != board[pos_x][pos_y].color):
             board[pos_x][pos_y] = Crossed_P()
         return 0
+
+    def check_en_passant_up_right(self, board):
+        pos_y = self.pos_y + 1
+        pos_x = self.pos_x + 1
+
+        if (pos_y <= 7 and pos_x <= 7 and "P" in board[pos_x][self.pos_y].name  and self.color != board[pos_x][pos_y].color):
+            board[pos_x][pos_y] = Crossed_P()
+        return 0
+
+
+    def check_en_passant_up_left(self, board):
+        pos_y = self.pos_y + 1
+        pos_x = self.pos_x - 1
+        test = pos_y <= 7 
+        test = pos_x >= 0 
+        test =  "P" in board[pos_x][self.pos_y].name  
+        test = (self.color != board[pos_x][pos_y].color)
+
+
+        if (pos_y <= 7 and pos_x >= 0 and "P" in board[pos_x][self.pos_y].name and self.color != board[pos_x][pos_y].color):
+            board[pos_x][pos_y] = Crossed_P()
+        return 0
+    
