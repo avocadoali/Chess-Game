bf9b2c19a34ea65daef5c630416e8e8c04aa3111
AlisArch <alfrednguyen02@gmail.com>
2022-06-07 23:58:10 +0200

    updated game flow

--- game_logic/game_logic.py
+++ game_logic/game_logic.py
@@ -10,9 +10,6 @@ class Game():
     def start(self):
         checkmate = False
 
-
-
-
         while (not checkmate):
             # print Board
             print("Player: " + self.current_player)
@@ -20,45 +17,45 @@ class Game():
             print("")
 
             # ask what field user wants to play
-            from_pos = input("Enter field coordinates(x,y):")
+            from_pos = input("Enter field coordinates(x,y): ")
 
             # input to number[0] number[1]
             numbers = [int(x) for x in from_pos.split() if x.isnumeric()]
-            print("")
 
             #check ic format is correct
             if len != 2 and not (numbers[0] >= 0 and numbers[0] <= 7 and numbers[1] >= 0 and numbers[1] <= 7):
                 print("")
                 print("Wrong coordinates")
-                p = input("Enter field coordinates(x,y):")
+                p = input("Enter field coordinates(x,y): ")
                 numbers = [int(x) for x in p.split() if x.isnumeric()]
                 print("")
 
+            # set x and y coordinates
             x = numbers[0]
             y = numbers[1]
 
+            # print board after piece is selected
             print("")
             self.board.check_for_field(x,y).print()
 
-            to_pos = input ("Enter field coordinates(x,y)")
+            # get coordinates where piece should be moved to
+            to_pos = input ("Enter field coordinates(x,y): ")
 
+            # input to number[0] number[1]
             numbers = [int(x) for x in to_pos.split() if x.isnumeric()]
-            print("")
 
             #check ic format is correct
             if len != 2 and not (numbers[0] >= 0 and numbers[0] <= 7 and numbers[1] >= 0 and numbers[1] <= 7):
                 print("")
                 print(numbers)
                 print("Wrong")
-                p = input("Enter field coordinates(x,y)")
+                p = input("Enter field coordinates(x,y:)")
                 numbers = [int(x) for x in p.split() if x.isnumeric()]
                 print("")
 
-
+            # set x and y coordinates where piece is going to be moved to
             to_x = numbers[0]
             to_y = numbers[1]
-
-            print( x, y, to_x, to_y )
 
             #move piece
             self.board.move_from_to(self.current_player , x,y, to_x, to_y)
